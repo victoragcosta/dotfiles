3971c1711c6df65564321d07a2d4c949120835c5
Victor André Gris Costa <victorandr98@gmail.com>
2024-06-08 23:45:50 -0300

    feat: create auto-importer and auto-saver

---
+++ save-files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ~/.zshrc ./.zshrc
+cp ~/.vimrc ./.vimrc
+cp ~/.gnupg/gpg.conf ./.gnupg/gpg.conf
+cp ~/.gnupg/gpg-agent.conf ./.gnupg/gpg-agent.conf
+cp ~/.config/nvim/* -r ./.config/nvim/
+cp ~/.config/rustfmt/rustfmt.toml ./.config/rustfmt/rustfmt.toml
+cp ~/.config/Google/AndroidStudio2023.3/studio64.vmoptions ./.config/Google/AndroidStudio2023.3/studio64.vmoptions
