9e2ee000d541571af28c5a9335dcba4e2ee2144f
Victor André Gris Costa <victorandr98@gmail.com>
2024-06-15 23:07:03 -0300

    feat (neovim): add debuggers, formatters, language servers, linters and linters

--- .config/nvim/lua/victoragc/plugins/debug.lua
+++ .config/nvim/lua/victoragc/plugins/debug.lua
@@ -1,6 +1,10 @@
 ---@type (string| LazyPluginSpec)[]
 local plugins = {
 	{
+		'microsoft/vscode-js-debug',
+		build = 'npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out',
+	},
+	{
 		-- NOTE: Yes, you can install new plugins here!
 		'mfussenegger/nvim-dap',
 		-- NOTE: And you can specify dependencies as well
@@ -11,12 +15,16 @@ local plugins = {
 			-- Required dependency for nvim-dap-ui
 			'nvim-neotest/nvim-nio',
 
+			-- Add virtual text
+			'theHamsta/nvim-dap-virtual-text',
+
 			-- Installs the debug adapters for you
 			'williamboman/mason.nvim',
 			'jay-babu/mason-nvim-dap.nvim',
 
 			-- Add your own debuggers here
 			-- 'leoluz/nvim-dap-go',
+			'mxsdev/nvim-dap-vscode-js',
 		},
 		config = function()
 			local dap = require 'dap'
@@ -35,16 +43,25 @@ local plugins = {
 				-- online, please don't ask me how to install them :)
 				ensure_installed = {
 					-- Update this to ensure that you have the debuggers for the langs you want
-					'delve',
 				},
 			}
 
 			-- Basic debugging keymaps, feel free to change to your liking!
-			vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+			vim.keymap.set(
+				'n',
+				'<F5>',
+				dap.continue,
+				{ desc = 'Debug: Start/Continue' }
+			)
 			vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
 			vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
 			vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-			vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+			vim.keymap.set(
+				'n',
+				'<leader>b',
+				dap.toggle_breakpoint,
+				{ desc = 'Debug: Toggle Breakpoint' }
+			)
 			vim.keymap.set('n', '<leader>B', function()
 				dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
 			end, { desc = 'Debug: Set Breakpoint' })
@@ -56,31 +73,93 @@ local plugins = {
 				-- Set icons to characters that are more likely to work in every terminal.
 				--    Feel free to remove or use ones that you like more! :)
 				--    Don't feel like these are good choices.
-				icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-				---@diagnostic disable-next-line: missing-fields
-				controls = {
-					icons = {
-						pause = '⏸',
-						play = '▶',
-						step_into = '⏎',
-						step_over = '⏭',
-						step_out = '⏮',
-						step_back = 'b',
-						run_last = '▶▶',
-						terminate = '⏹',
-						disconnect = '⏏',
-					},
-				},
+				-- icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+				-- ---@diagnostic disable-next-line: missing-fields
+				-- controls = {
+				-- 	icons = {
+				-- 		pause = "⏸",
+				-- 		play = "▶",
+				-- 		step_into = "⏎",
+				-- 		step_over = "⏭",
+				-- 		step_out = "⏮",
+				-- 		step_back = "b",
+				-- 		run_last = "▶▶",
+				-- 		terminate = "⏹",
+				-- 		disconnect = "⏏",
+				-- 	},
+				-- },
 			}
 
 			-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-			vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+			vim.keymap.set(
+				'n',
+				'<F7>',
+				dapui.toggle,
+				{ desc = 'Debug: See last session result.' }
+			)
 
 			dap.listeners.after.event_initialized['dapui_config'] = dapui.open
 			dap.listeners.before.event_terminated['dapui_config'] = dapui.close
 			dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
 			-- Install specific config
+			require('dap-vscode-js').setup {
+				-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+				-- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug", -- Path to vscode-js-debug installation.
+				-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+				adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal' }, -- which adapters to register in nvim-dap
+				-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+				-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+				-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+			}
+
+			local js_dap_languages = {
+				'typescript',
+				'javascript',
+				'typescriptreact',
+				'javascriptreact',
+				'svelte',
+				-- 'vue',
+			}
+
+			for _, language in ipairs(js_dap_languages) do
+				require('dap').configurations[language] = {
+					{
+						type = 'pwa-node',
+						request = 'launch',
+						name = 'Launch file',
+						program = '${file}',
+						cwd = '${workspaceFolder}',
+					},
+					{
+						type = 'pwa-node',
+						request = 'attach',
+						name = 'Attach',
+						processId = require('dap.utils').pick_process,
+						cwd = '${workspaceFolder}',
+					},
+					{
+						type = 'pwa-chrome',
+						request = 'launch',
+						name = 'Launch & Debug Chrome',
+						url = function()
+							local co = coroutine.running()
+							return coroutine.create(function()
+								vim.ui.input({
+									prompt = 'Enter URL: ',
+									default = 'http://localhost:4000',
+								}, function(url)
+									if url == nil or url == '' then
+										return
+									else
+										coroutine.resume(co, url)
+									end
+								end)
+							end)
+						end,
+					},
+				}
+			end
 		end,
 	},
 }
--- .config/nvim/lua/victoragc/plugins/lsp.lua
+++ .config/nvim/lua/victoragc/plugins/lsp.lua
@@ -47,7 +47,10 @@ local plugins = {
 			--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
 			--    function will be executed to configure the current buffer
 			vim.api.nvim_create_autocmd('LspAttach', {
-				group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+				group = vim.api.nvim_create_augroup(
+					'kickstart-lsp-attach',
+					{ clear = true }
+				),
 				callback = function(event)
 					-- NOTE: Remember that Lua is a real programming language, and as such it is possible
 					-- to define small helper and utility functions so you don't have to repeat yourself.
@@ -55,33 +58,62 @@ local plugins = {
 					-- In this case, we create a function that lets us more easily define mappings specific
 					-- for LSP related items. It sets the mode, buffer and description for us each time.
 					local map = function(keys, func, desc)
-						vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+						vim.keymap.set(
+							'n',
+							keys,
+							func,
+							{ buffer = event.buf, desc = 'LSP: ' .. desc }
+						)
 					end
 
 					-- Jump to the definition of the word under your cursor.
 					--  This is where a variable was first declared, or where a function is defined, etc.
 					--  To jump back, press <C-t>.
-					map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+					map(
+						'gd',
+						require('telescope.builtin').lsp_definitions,
+						'[G]oto [D]efinition'
+					)
 
 					-- Find references for the word under your cursor.
-					map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+					map(
+						'gr',
+						require('telescope.builtin').lsp_references,
+						'[G]oto [R]eferences'
+					)
 
 					-- Jump to the implementation of the word under your cursor.
 					--  Useful when your language has ways of declaring types without an actual implementation.
-					map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+					map(
+						'gI',
+						require('telescope.builtin').lsp_implementations,
+						'[G]oto [I]mplementation'
+					)
 
 					-- Jump to the type of the word under your cursor.
 					--  Useful when you're not sure what type a variable is and you want to see
 					--  the definition of its *type*, not where it was *defined*.
-					map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+					map(
+						'<leader>D',
+						require('telescope.builtin').lsp_type_definitions,
+						'Type [D]efinition'
+					)
 
 					-- Fuzzy find all the symbols in your current document.
 					--  Symbols are things like variables, functions, types, etc.
-					map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+					map(
+						'<leader>ds',
+						require('telescope.builtin').lsp_document_symbols,
+						'[D]ocument [S]ymbols'
+					)
 
 					-- Fuzzy find all the symbols in your current workspace.
 					--  Similar to document symbols, except searches over your entire project.
-					map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+					map(
+						'<leader>ws',
+						require('telescope.builtin').lsp_dynamic_workspace_symbols,
+						'[W]orkspace [S]ymbols'
+					)
 
 					-- Rename the variable under your cursor.
 					--  Most Language Servers support renaming across files, etc.
@@ -105,8 +137,13 @@ local plugins = {
 					--
 					-- When you move your cursor, the highlights will be cleared (the second autocommand).
 					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.server_capabilities.documentHighlightProvider then
-						local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+					if
+						client and client.server_capabilities.documentHighlightProvider
+					then
+						local highlight_augroup = vim.api.nvim_create_augroup(
+							'kickstart-lsp-highlight',
+							{ clear = false }
+						)
 						vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
 							buffer = event.buf,
 							group = highlight_augroup,
@@ -120,10 +157,16 @@ local plugins = {
 						})
 
 						vim.api.nvim_create_autocmd('LspDetach', {
-							group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+							group = vim.api.nvim_create_augroup(
+								'kickstart-lsp-detach',
+								{ clear = true }
+							),
 							callback = function(event2)
 								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+								vim.api.nvim_clear_autocmds {
+									group = 'kickstart-lsp-highlight',
+									buffer = event2.buf,
+								}
 							end,
 						})
 					end
@@ -132,7 +175,11 @@ local plugins = {
 					-- code, if the language server you are using supports them
 					--
 					-- This may be unwanted, since they displace some of your code
-					if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+					if
+						client
+						and client.server_capabilities.inlayHintProvider
+						and vim.lsp.inlay_hint
+					then
 						map('<leader>th', function()
 							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
 							-- notify the user the state of inlay hints
@@ -151,7 +198,11 @@ local plugins = {
 			--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 			--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+			capabilities = vim.tbl_deep_extend(
+				'force',
+				capabilities,
+				require('cmp_nvim_lsp').default_capabilities()
+			)
 
 			-- Enable the following language servers
 			--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -163,19 +214,26 @@ local plugins = {
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				-- clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
-				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+				-- See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
-				--
 
+				-- HTML
+				html = {},
+				emmet_language_server = {},
+				-- CSS
+				css_variables = {},
+				cssls = {},
+				tailwindcss = {},
+				-- TS, JS and related
+				tsserver = {},
+				svelte = {},
+				eslint = {},
+				graphql = {},
+				rust_analyzer = {},
 				lua_ls = {
 					-- cmd = {...},
 					-- filetypes = { ...},
@@ -205,8 +263,12 @@ local plugins = {
 			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				'stylua', -- Used to format Lua code
+				'prettier',
+				'prettierd',
 			})
-			require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+			require('mason-tool-installer').setup {
+				ensure_installed = ensure_installed,
+			}
 
 			require('mason-lspconfig').setup {
 				handlers = {
@@ -215,7 +277,12 @@ local plugins = {
 						-- This handles overriding only values explicitly passed
 						-- by the server configuration above. Useful when disabling
 						-- certain features of an LSP (for example, turning off formatting for tsserver)
-						server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+						server.capabilities = vim.tbl_deep_extend(
+							'force',
+							{},
+							capabilities,
+							server.capabilities or {}
+						)
 						require('lspconfig')[server_name].setup(server)
 					end,
 				},
@@ -254,7 +321,13 @@ local plugins = {
 				--
 				-- You can use a sub-list to tell conform to run *until* a formatter
 				-- is found.
-				-- javascript = { { "prettierd", "prettier" } },
+				javascript = { { 'prettierd', 'prettier' } },
+				typescript = { { 'prettierd', 'prettier' } },
+				javascriptreact = { { 'prettierd', 'prettier' } },
+				typescriptreact = { { 'prettierd', 'prettier' } },
+				svelte = { { 'prettierd', 'prettier' } },
+
+				rust = { 'rustfmt' },
 			},
 		},
 	},
@@ -264,7 +337,13 @@ local plugins = {
 		config = function()
 			local lint = require 'lint'
 			lint.linters_by_ft = {
-				markdown = { 'markdownlint' },
+				markdown = { 'markdownlint', 'cspell' },
+				javascript = { 'eslint_d', 'cspell' },
+				typescript = { 'eslint_d', 'cspell' },
+				javascriptreact = { 'eslint_d', 'cspell' },
+				typescriptreact = { 'eslint_d', 'cspell' },
+				svelte = { 'eslint_d', 'cspell' },
+				rust = { 'cspell' },
 			}
 
 			-- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -302,12 +381,15 @@ local plugins = {
 			-- Create autocommand which carries out the actual linting
 			-- on the specified events.
 			local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-			vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-				group = lint_augroup,
-				callback = function()
-					require('lint').try_lint()
-				end,
-			})
+			vim.api.nvim_create_autocmd(
+				{ 'BufEnter', 'BufWritePost', 'InsertLeave' },
+				{
+					group = lint_augroup,
+					callback = function()
+						require('lint').try_lint()
+					end,
+				}
+			)
 		end,
 	},
 	{ -- Autocompletion
@@ -424,7 +506,17 @@ local plugins = {
 		'nvim-treesitter/nvim-treesitter',
 		build = ':TSUpdate',
 		opts = {
-			ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+			ensure_installed = {
+				'bash',
+				'c',
+				'diff',
+				'html',
+				'lua',
+				'luadoc',
+				'markdown',
+				'vim',
+				'vimdoc',
+			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
