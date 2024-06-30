d800f65ea7b26c845ae9a1171155e34b5e2b4c4a
Victor André Gris Costa <victorandr98@gmail.com>
2024-06-30 09:17:09 -0300

    feat(neovim): add a decent task manager

---
+++ .config/nvim/lua/victoragc/plugins/overseer.lua
@@ -0,0 +1,21 @@
+---@type (string | LazyPluginSpec)[]
+local plugins = {
+	{
+		'stevearc/overseer.nvim',
+		opts = {},
+		keys = {
+			{
+				'<leader>xt',
+				'<Cmd>OverseerRun<CR>',
+				desc = 'E[x]ecute [t]ask',
+			},
+			{
+				'<leader>tt',
+				'<Cmd>OverseerToggle<CR>',
+				desc = '[T]oggle [t]asklist',
+			},
+		},
+	},
+}
+
+return plugins
