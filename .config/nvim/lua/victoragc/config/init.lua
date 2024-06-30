61ec07be5b0f86106dfa205ff4ed6ce9f528324d
Victor André Gris Costa <victorandr98@gmail.com>
2024-06-30 09:20:54 -0300

    refactor(neovim): remove module that I made to patch lack of task plugin

--- .config/nvim/lua/victoragc/config/init.lua
+++ .config/nvim/lua/victoragc/config/init.lua
@@ -2,6 +2,3 @@ require 'victoragc.config.config'
 require 'victoragc.config.firenvim'
 require 'victoragc.config.keymaps'
 require 'victoragc.config.autocommands'
-
--- Add my own plugin like modules
-require 'victoragc.module.command-hotkey'
--- .config/nvim/lua/victoragc/module/init.lua
+++
@@ -1,1 +0,0 @@
-require 'victoragc.module.command-hotkey'
