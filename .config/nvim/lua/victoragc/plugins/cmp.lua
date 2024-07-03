0f1c842daec66020f308de4db8f800ae150b268a
Victor André Gris Costa <victorandr98@gmail.com>
2024-07-03 18:47:05 -0300

    refactor(neovim): update completion configs

--- .config/nvim/lua/victoragc/plugins/cmp.lua
+++ .config/nvim/lua/victoragc/plugins/cmp.lua
@@ -51,7 +51,7 @@ local plugins = {
 						luasnip.lsp_expand(args.body)
 					end,
 				},
-				completion = { completeopt = 'menu,menuone,noinsert' },
+				completion = { completeopt = 'menu,menuone,noinsert,popup' },
 
 				-- For an understanding of why these mappings were
 				-- chosen, you will need to read `:help ins-completion`
