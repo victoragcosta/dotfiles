c906eddbd72a382cc2d1d2d70eadef12f8538d40
Victor André Gris Costa <victorandr98@gmail.com>
2024-07-03 10:20:40 -0300

    refactor(neovim): improve deadcolumn configs

--- .config/nvim/lua/victoragc/plugins/deadcolumn.lua
+++ .config/nvim/lua/victoragc/plugins/deadcolumn.lua
@@ -1,7 +1,18 @@
 ---@type (string | LazyPluginSpec)[]
 local plugins = {
 	-- Shows a colored column when you get close to it or past it
-	{ 'Bekaboo/deadcolumn.nvim' },
+	{
+		'Bekaboo/deadcolumn.nvim',
+		opts = {
+			scope = 'visible',
+			modes = function(mode)
+				return mode:find '^[nictRss\x13]' ~= nil
+			end,
+			extra = {
+				follow_tw = '+1',
+			},
+		},
+	},
 }
 
 return plugins
