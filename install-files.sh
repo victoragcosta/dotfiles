2f0b105ba941de2f98c1986d290eb32b4f3485d3
Victor André Gris Costa <victorandr98@gmail.com>
2024-07-14 13:17:30 -0300

    fix: fix dotfiles installer

--- install-files.sh
+++ install-files.sh
@@ -5,6 +5,8 @@ cp ./.vimrc ~/.vimrc
 cp ./.gnupg/gpg.conf ~/.gnupg/gpg.conf
 cp ./.gnupg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 cp ./.config/nvim/* -r ~/.config/nvim/
+cp ./.config/nvim/.stylua.toml ~/.config/nvim/
+cp ./.config/nvim/.gitignore ~/.config/nvim/
 cp ./.config/rustfmt/rustfmt.toml ~/.config/rustfmt/rustfmt.toml
 cp ./.config/cspell/* ~/.config/cspell/
 cp ./.config/Google/AndroidStudio2023.3/studio64.vmoptions ~/.config/Google/AndroidStudio2023.3/studio64.vmoptions
