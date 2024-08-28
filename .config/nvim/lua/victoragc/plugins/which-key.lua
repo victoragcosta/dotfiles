8a50742810f2532542237ec392a192fc03022bab
Victor André Gris Costa <victorandr98@gmail.com>
2024-08-28 15:31:07 -0300

    fix(neovim): fix whichkey config for updated version

--- .config/nvim/lua/victoragc/plugins/which-key.lua
+++ .config/nvim/lua/victoragc/plugins/which-key.lua
@@ -5,36 +5,26 @@ local plugins = {
 		event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
 		config = function() -- This is the function that runs, AFTER loading
 			require('which-key').setup {
-				triggers_blacklist = {
-					i = { 'f', 'j', 'k', '<C-CR>' },
-					v = { 'f', 'j', 'k' },
+				triggers = {
+					{ '<auto>', mode = 'nxsot' },
 				},
 			}
 
 			-- Document existing key chains
-			require('which-key').register {
-				['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-				['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-				['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-				['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-				['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-				['<leader>tw'] = {
-					name = '[T]oggle Tail[w]ind',
-					_ = 'which_key_ignore',
-				},
-				['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-				['<leader>o'] = { name = '[O]il file manager', _ = 'which_key_ignore' },
-				['<leader>x'] = { name = 'E[x]ecute', _ = 'which_key_ignore' },
-				['<leader>xc'] = { name = '[C]olor', _ = 'which_key_ignore' },
-				['<leader>d!'] = {
-					name = '[D]ocument [!]force',
-					_ = 'which_key_ignore',
-				},
+			require('which-key').add {
+				{ '<leader>c', group = '[C]ode' },
+				{ '<leader>d', group = '[D]ocument' },
+				{ '<leader>d!', group = '[D]ocument [!]force' },
+				{ '<leader>h', group = 'Git [H]unk' },
+				{ '<leader>o', group = '[O]il file manager' },
+				{ '<leader>r', group = '[R]ename' },
+				{ '<leader>s', group = '[S]earch' },
+				{ '<leader>t', group = '[T]oggle' },
+				{ '<leader>tw', group = '[T]oggle Tail[w]ind' },
+				{ '<leader>x', group = 'E[x]ecute' },
+				{ '<leader>xc', group = '[C]olor' },
+				{ '<leader>h', group = 'Git [H]unk', mode = 'v' },
 			}
-			-- visual mode
-			require('which-key').register({
-				['<leader>h'] = { 'Git [H]unk' },
-			}, { mode = 'v' })
 		end,
 	},
 }
