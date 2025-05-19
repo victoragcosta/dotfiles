fd262b3c4fe21b13a442b80bfb0a2b738a8e89ee
Victor André Gris Costa <victorandr98@gmail.com>
2025-05-19 15:57:22 -0300

    fix(neovim): disable lua rocks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -74,6 +74,9 @@ require('lazy').setup({
 	--    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 	{ import = 'victoragc.plugins' },
 }, {
+	rocks = {
+		enabled = false,
+	},
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
 		-- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
