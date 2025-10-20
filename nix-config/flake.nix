b9f23ad58b3833e42a3bb54690a4450ec8ec253e
Victor André Gris Costa <victorandr98@gmail.com>
2025-10-20 19:21:46 -0300

    style(nix): format

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -10,13 +10,14 @@
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     plasma-manager = {
-      url="github:nix-community/plasma-manager";
-    inputs.nixpkgs.follows = "nixpkgs";
-    inputs.home-manager.follows = "home-manager";
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, plasma-manager, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, plasma-manager, ...
+    }@inputs:
     let
       system = "x86_64-linux";
       make-pkgs-unstable = args:
@@ -36,11 +37,13 @@
           # so the old configuration file still takes effect
           ./nixos/configuration.nix
           ./home/default.nix
-          home-manager.nixosModules.home-manager {
+          home-manager.nixosModules.home-manager
+          {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.cubo = ./home/home.nix;
-            home-manager.sharedModules =[plasma-manager.homeModules.plasma-manager];
+            home-manager.sharedModules =
+              [ plasma-manager.homeModules.plasma-manager ];
           }
         ];
       };
