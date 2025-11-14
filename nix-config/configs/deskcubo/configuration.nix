ceb5b4dfe9578d0d02c81c4f27a50de4be5b0747
Victor André Gris Costa <victorandr98@gmail.com>
2025-11-14 16:47:29 -0300

    refactor(nix): refactor out common settings from both configs

--- nix-config/configs/deskcubo/configuration.nix
+++ nix-config/configs/deskcubo/configuration.nix
@@ -6,13 +6,9 @@
 let unstable-pkgs = make-pkgs-unstable { config = { allowUnfree = true; }; };
 in {
   imports = [ # Include the results of the hardware scan.
+    ../base.nix
     ./hardware-configuration.nix
   ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.memtest86.enable = true;
 
   fileSystems."/mnt/sata-ssd" = {
     device = "/dev/disk/by-uuid/9bd6eb78-e750-4b56-820d-301472544d56";
@@ -30,122 +26,6 @@ in {
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/Sao_Paulo";
-
-  # Select internationalisation properties.
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      # LANGUAGE = ""; # fix ç in us-intl
-      LC_ADDRESS = "pt_BR.UTF-8";
-      LC_COLLATE = "en_US.UTF-8";
-      LC_CTYPE = "pt_BR.UTF-8"; # fix ç in us-intl
-      LC_IDENTIFICATION = "pt_BR.UTF-8";
-      LC_MEASUREMENT = "pt_BR.UTF-8";
-      LC_MESSAGES = "en_US.UTF-8";
-      LC_MONETARY = "pt_BR.UTF-8";
-      LC_NAME = "pt_BR.UTF-8";
-      LC_NUMERIC = "pt_BR.UTF-8";
-      LC_PAPER = "pt_BR.UTF-8";
-      LC_TELEPHONE = "pt_BR.UTF-8";
-      LC_TIME = "pt_BR.UTF-8";
-    };
-  };
-
-  # Cedilla fix
-  environment.variables = {
-    GTK_IM_MODULE = "cedilla";
-    QT_IM_MODULE = "cedilla";
-  };
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure console keymap
-  console.keyMap = "us-acentos";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable bluetooth support
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-  hardware.bluetooth.settings = { General = { Experimental = true; }; };
-
-  # Experimental
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    zsh
-    oh-my-zsh
-    vim
-    git
-    wget
-    gnutar
-    usbutils
-    lazygit
-    stow
-    ghostty
-    brave
-    vlc
-    torrential
-    gparted
-    kdePackages.kalk
-    gamemode
-    quickemu
-  ];
-
-  # setup virtualisation
-  virtualisation.spiceUSBRedirection.enable = true;
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Auto mount drives
-  services.udisks2.enable = true;
-
-  # Configure and install udev related packages
-  services.udev.packages = with pkgs; [ vial via ];
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Enable fwupd to update BIOS and other firmwares
-  services.fwupd.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
