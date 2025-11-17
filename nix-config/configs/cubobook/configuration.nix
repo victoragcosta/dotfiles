0f7a0f0a3635d6f56de817ce4f1eecc75e21bbee
Victor André Gris Costa <victorandr98@gmail.com>
2025-11-17 18:34:11 -0300

    fix(nix): fix nvidia config to actually install drivers

--- nix-config/configs/cubobook/configuration.nix
+++ nix-config/configs/cubobook/configuration.nix
@@ -26,6 +26,9 @@ in {
   # configure Nvidia GPU
   hardware.graphics.enable = true;
 
+  # WHY THE FUCK DO I NEED TO ADD VIDEO DRIVERS TO XSERVER IF I'M USING WAYLAND?!
+  services.xserver.videoDrivers = [ "nvidia" ];
+
   hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
@@ -38,7 +41,7 @@ in {
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
+    powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
