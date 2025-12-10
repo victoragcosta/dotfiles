ab76a29b1c5782b82d8cc2662bdee23b849940ba
Victor André Gris Costa <victorandr98@gmail.com>
2025-12-10 08:07:38 -0300

    feat(nix): add sunshine to cubobook

--- nix-config/configs/cubobook/configuration.nix
+++ nix-config/configs/cubobook/configuration.nix
@@ -13,6 +13,8 @@ in {
   networking.hostName = "cubobook"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  services.sunshine.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
