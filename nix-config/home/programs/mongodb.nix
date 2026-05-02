71d9cd45a5aaa35d31c905119cfe965b1ab20fcc
Victor André Gris Costa <victorandr98@gmail.com>
2026-05-02 12:15:53 -0300

    fix(nix): correctly set mongodb package option

--- nix-config/home/programs/mongodb.nix
+++ nix-config/home/programs/mongodb.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }: {
   config = lib.mkIf config.services.mongodb.enable {
-    package = pkgs.mongodb-ce;
+    services.mongodb = { package = pkgs.mongodb-ce; };
     users.users.cubo.packages = with pkgs;
       [
         # mongodb-ce
