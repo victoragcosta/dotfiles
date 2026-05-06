832f4ba3ad0063f70f13c9bcfe3aab485ef7504d
Victor André Gris Costa <victorandr98@gmail.com>
2026-05-06 12:40:04 -0300

    refactor(nix): remove kernel patch for SteamVR

--- nix-config/home/programs/steam.nix
+++ nix-config/home/programs/steam.nix
@@ -5,15 +5,15 @@
 
     # Allows steam to run SteamVR
     # See more at https://wiki.nixos.org/wiki/VR#SteamVR
-    boot.kernelPatches = [{
-      name = "amdgpu-ignore-ctx-privileges";
-      patch = pkgs.fetchpatch {
-        name = "cap_sys_nice_begone.patch";
-        url =
-          "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
-        hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
-      };
-    }];
+    # boot.kernelPatches = [{
+    #   name = "amdgpu-ignore-ctx-privileges";
+    #   patch = pkgs.fetchpatch {
+    #     name = "cap_sys_nice_begone.patch";
+    #     url =
+    #       "https://github.com/Frogging-Family/community-patches/raw/master/linux61-tkg/cap_sys_nice_begone.mypatch";
+    #     hash = "sha256-Y3a0+x2xvHsfLax/uwycdJf3xLxvVfkfDVqjkxNaYEo=";
+    #   };
+    # }];
 
     # Add steam
     programs.steam =
