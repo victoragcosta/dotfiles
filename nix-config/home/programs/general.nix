a073e27066b1643ff9b12a8918478425a08582e1
Victor André Gris Costa <victorandr98@gmail.com>
2026-05-10 12:50:30 -0300

    feat(nix): add solaar for logitech devices management

--- nix-config/home/programs/general.nix
+++ nix-config/home/programs/general.nix
@@ -29,7 +29,43 @@
     easyeffects
     libreoffice-qt6-fresh
     yt-dlp
+    # For configuring logitech devices
+    solaar
   ];
+
+  services.udev.extraRules = ''
+    # This rule was added by Solaar.
+    #
+    # Allows non-root users to have raw access to Logitech devices.
+    # Allowing users to write to the device is potentially dangerous
+    # because they could perform firmware updates.
+
+    ACTION == "remove", GOTO="solaar_end"
+    SUBSYSTEM != "hidraw", GOTO="solaar_end"
+
+    # USB-connected Logitech receivers and devices
+    ATTRS{idVendor}=="046d", GOTO="solaar_apply"
+
+    # Lenovo nano receiver
+    ATTRS{idVendor}=="17ef", ATTRS{idProduct}=="6042", GOTO="solaar_apply"
+
+    # Bluetooth-connected Logitech devices
+    KERNELS == "0005:046D:*", GOTO="solaar_apply"
+
+    GOTO="solaar_end"
+
+    LABEL="solaar_apply"
+
+    # Allow any seated user to access the receiver.
+    # uaccess: modern ACL-enabled udev
+    TAG+="uaccess"
+
+    # Grant members of the "plugdev" group access to receiver (useful for SSH users)
+    #MODE="0660", GROUP="plugdev"
+
+    LABEL="solaar_end"
+    # vim: ft=udevrules
+  '';
 
   # Fix libreoffice using x11 instead of wayland
   environment.variables = { QT_QPA_PLATFORM = "wayland"; };
