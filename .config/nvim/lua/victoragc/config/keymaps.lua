b9f6ecb461cc79e9701766025eafe34094e1f557
Victor André Gris Costa <victorandr98@gmail.com>
2026-07-06 19:11:59 -0300

    fix(neovim): substitute deprecated functions

--- .config/nvim/lua/victoragc/config/keymaps.lua
+++ .config/nvim/lua/victoragc/config/keymaps.lua
@@ -11,18 +11,12 @@ vim.keymap.set(
 )
 
 -- Diagnostic keymaps
-vim.keymap.set(
-	'n',
-	'[d',
-	vim.diagnostic.goto_prev,
-	{ desc = 'Go to previous [D]iagnostic message' }
-)
-vim.keymap.set(
-	'n',
-	']d',
-	vim.diagnostic.goto_next,
-	{ desc = 'Go to next [D]iagnostic message' }
-)
+vim.keymap.set('n', '[d', function()
+	vim.diagnostic.jump { count = -1, float = true }
+end, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', function()
+	vim.diagnostic.jump { count = 1, float = true }
+end, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set(
 	'n',
 	'<leader>e',
