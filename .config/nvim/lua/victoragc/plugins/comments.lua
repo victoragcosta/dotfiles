6f8ed7bf30d3bf633603927a46b1a3646cdb127a
Victor André Gris Costa <victorandr98@gmail.com>
2026-07-06 19:11:29 -0300

    feat(neovim): add support for auto commenting React files

--- .config/nvim/lua/victoragc/plugins/comments.lua
+++ .config/nvim/lua/victoragc/plugins/comments.lua
@@ -1,7 +1,16 @@
 require 'url'
 
-vim.pack.add(
+vim.pack.add {
 	-- "gc" to comment visual regions/lines
-	{ github 'numToStr/Comment.nvim' }
-)
-require('Comment').setup()
+	github 'numToStr/Comment.nvim',
+	-- deal with hard files like tsx/jsx
+	github 'JoosepAlviste/nvim-ts-context-commentstring',
+}
+
+require('ts_context_commentstring').setup {
+	enable_autocmd = false,
+}
+---@diagnostic disable-next-line: missing-fields
+require('Comment').setup {
+	pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+}
