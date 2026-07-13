a5cee0b3b6d47e0ed3aa67e1ba1799d94a69e5a7
Victor André Gris Costa <victorandr98@gmail.com>
2026-07-13 18:46:19 -0300

    feat(neovim): add a better signature handler for better overload handling

--- .config/nvim/lua/victoragc/plugins/cmp.lua
+++ .config/nvim/lua/victoragc/plugins/cmp.lua
@@ -1,41 +1,55 @@
 require 'url'
 
-vim.pack.add({
+vim.pack.add {
 	{ src = github 'saghen/blink.cmp', version = vim.version.range '1.*' },
 	{ src = github 'L3MON4D3/LuaSnip', version = vim.version.range '2.*' },
+	github 'Issafalcon/lsp-overloads.nvim',
 	github 'barrettruth/blink-cmp-ghostty',
-})
+}
 
 require('luasnip').setup()
-require('blink.cmp').setup({
-keymap= {
+require('blink.cmp').setup {
+	keymap = {
 		preset = 'default',
-				-- Select the next item with j as in going down
-				['<C-j>'] = {'select_next', 'fallback'},
-				-- Select the previous item with k as in going up
-				['<C-k>'] = {'select_prev', 'fallback'},
+		-- Select the next item with j as in going down
+		['<C-j>'] = { 'select_next', 'fallback' },
+		-- Select the previous item with k as in going up
+		['<C-k>'] = { 'select_prev', 'fallback' },
 
-				-- Scroll the documentation window [b]ack / [f]orward
-				['<C-b>'] = {'scroll_documentation_up', 'fallback'},
-				['<C-f>'] = {'scroll_documentation_down', 'fallback'},
+		-- Scroll the documentation window [b]ack / [f]orward
+		['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+		['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
 
-				-- ['<C-l>'] = cmp.mapping(function()
-				-- 	if luasnip.expand_or_locally_jumpable() then
-				-- 		luasnip.expand_or_jump()
-				-- 	end
-				-- end, { 'i', 's' }),
-				-- ['<C-h>'] = cmp.mapping(function()
-				-- 	if luasnip.locally_jumpable(-1) then
-				-- 		luasnip.jump(-1)
-				-- 	end
-				-- end, { 'i', 's' }),
+		-- Scroll the signature window
+		-- ['<C-p>'] = { 'scroll_signature_up', 'fallback' },
+		-- ['<C-n>'] = { 'scroll_signature_down', 'fallback' },
+		-- Show the signature
+		-- ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
+
+		-- ['<C-l>'] = cmp.mapping(function()
+		-- 	if luasnip.expand_or_locally_jumpable() then
+		-- 		luasnip.expand_or_jump()
+		-- 	end
+		-- end, { 'i', 's' }),
+		-- ['<C-h>'] = cmp.mapping(function()
+		-- 	if luasnip.locally_jumpable(-1) then
+		-- 		luasnip.jump(-1)
+		-- 	end
+		-- end, { 'i', 's' }),
 	},
 	appearance = {
-		nerd_font_variant = 'mono'
+		nerd_font_variant = 'mono',
 	},
 	completion = {
-		documentation = { auto_show = true, auto_show_delay_ms = 500},
-		menu = {draw = { columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind" } },}}
+		documentation = { auto_show = true, auto_show_delay_ms = 500 },
+		menu = {
+			draw = {
+				columns = {
+					{ 'label', 'label_description', gap = 1 },
+					{ 'kind_icon', 'kind' },
+				},
+			},
+		},
 	},
 	sources = {
 		default = { 'lsp', 'path', 'snippets', 'path', 'ghostty' },
@@ -52,6 +66,16 @@ keymap= {
 	fuzzy = {
 		implementation = 'prefer_rust_with_warning',
 	},
-	signature = { enabled = true },
-})
+	signature = { enabled = false },
+}
 
+require('lsp-overloads').setup {
+	display_automatically = true,
+}
+
+vim.keymap.set(
+	{ 'n', 'i' },
+	'<A-s>',
+	'<cmd>LspOverloads signature<CR>',
+	{ silent = true }
+)
